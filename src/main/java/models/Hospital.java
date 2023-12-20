75d5231287ea8cdfa4495a1f6896a538bd7a8e08
Janet_Mensah <janetmensah60@gmail.com>
2023-12-20 09:52:38 +0000

    created Hospital pojo class

--- src/main/java/models/Hospital.java
+++ src/main/java/models/Hospital.java
@@ -1,4 +1,68 @@
 package models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table (name="hospital")
 public class Hospital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private  String address;
+
+    @Column
+    private List <Department> departments;
+
+
+    public Hospital(String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.departments= new ArrayList<>();
+    }
+
+
+    public Hospital() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
 }
