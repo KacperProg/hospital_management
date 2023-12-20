1195f55319362880f181731ec2609a9d53edea67
Rayster Ramos <rjrfrst@gmail.com>
2023-12-20 09:38:15 +0000

    created POJO for Doctor

--- src/main/java/models/Doctor.java
+++ src/main/java/models/Doctor.java
@@ -1,4 +1,57 @@
 package models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "doctors")
 public class Doctor {
-}
+
+    //Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+//    refactor
+    private String name;
+
+    @Column
+//    refactor
+    private List<Department> departments;
+
+    //Constructor
+    public Doctor(String name) {
+        this.name = name;
+        this.departments = new ArrayList<>();
+    }
+
+    //Empty constructor
+    public Doctor(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Department> getDepartments() {
+        return departments;
+    }
+    public void setDepartments(List<Department> departments) {
+        this.departments = departments;
+    }
+
+} //Last
